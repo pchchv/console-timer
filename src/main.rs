fb9dd7cda906afd7407c2506cb0ab2dd03ce444f
pchchv <ipchchv@gmail.com>
2025-02-10 11:25:27 +0300

    Add usage message for console-timer when insufficient arguments are provided

--- src/main.rs
+++ src/main.rs
@@ -2,4 +2,8 @@ use std::env;
 
 fn main() {
     let args : Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        println!("Usage: console-timer [time in seconds]");
+        return;
+    }
 }
